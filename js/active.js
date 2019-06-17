d851a4ca3587dce0abcf7a134b4370a37d95b267
Frontend2 <frontend2@eyepaycard.co>
2019-06-17 09:41:45 -0500

    - Indexación a Dashboard. - Implementación de Animaciones. - Error 404 en Términos y Condiciones. - Cambio de Iconos de redes sociales. - Cambio de Layout en Resoluciones entre 640 y 768px para hacerlas mas visibles en dispositivos mobiles. - Cambio de Icono de verificación en el carrusel. - Cambio del  Loader, ahora es más grande.

--- js/active.js
+++ js/active.js
@@ -45,79 +45,9 @@
         });
     }
 
-    // :: 2.0 Slick Active Code
-    if ($.fn.slick) {
-        $('.slider-for').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 500,
-            arrows: false,
-            fade: true,
-            asNavFor: '.slider-nav'
-        });
-        $('.slider-nav').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            speed: 500,
-            asNavFor: '.slider-for',
-            dots: true,
-            centerMode: true,
-            focusOnSelect: true,
-            slide: 'div',
-            autoplay: true,
-            centerMode: true,
-            centerPadding: '30px',
-            mobileFirst: true,
-            prevArrow: '<i class="fa fa-angle-left"></i>',
-            nextArrow: '<i class="fa fa-angle-right"></i>'
-        });
-    }
 
-    // :: 3.0 Footer Reveal Active Code
-    if ($.fn.footerReveal) {
-        $('footer').footerReveal({
-            shadow: true,
-            shadowOpacity: 0.3,
-            zIndex: -101
-        });
-    }
 
-    // :: 4.0 ScrollUp Active Code
-    if ($.fn.scrollUp) {
-        $.scrollUp({
-            scrollSpeed: 1500,
-            scrollText: '<i class="fa fa-angle-up arrow"></i>'
-        });
-    }
 
-    // :: 5.0 CounterUp Active Code
-    if ($.fn.counterUp) {
-        $('.counter').counterUp({
-            delay: 10,
-            time: 2000
-        });
-    }
-
-    // :: 6.0 onePageNav Active Code
-    if ($.fn.onePageNav) {
-        $('#nav').onePageNav({
-            currentClass: 'active',
-            scrollSpeed: 2000,
-            easing: 'easeOutQuad'
-        });
-    }
-
-    // :: 7.0 Magnific-popup Video Active Code
-    if ($.fn.magnificPopup) {
-        $('.video_btn').magnificPopup({
-            disableOn: 0,
-            type: 'iframe',
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: true,
-            fixedContentPos: false
-        });
-    }
 
     $('a[href="#"]').click(function ($) {
         $.preventDefault()
@@ -238,10 +168,14 @@ $(window).scroll(function(){
 
 
 $(window).scroll(function(){
-    if ($(this).scrollTop()>=$('#home').position().top) {
+    if ($(this).scrollTop()>=$('#operamos').position().top) {
+        $(".link-show").addClass("blue-navbar");
+        $(".explorar").addClass("blue-navbar");
+        $("hr").addClass("hr-blue");
+        $(".logo").addClass("logo-blue");
+        $(".navbar-toggler-icon").addClass("toggle-blue")
     } 
 });
-
 
 // A partir de 991px empieza a aparecer un menu responsivo
 $(".navbar-toggler").click(function(){
@@ -371,24 +305,17 @@ $.fn.check = function checking() {
 }
 
 
-$(window).scroll(function(){
-    if  ($('#header').offset().top > 100){
-        alert('Hola')
-        // setTimeout( function(){
-        //         $('html').css("overflow-y", "hidden");
-        //     },0)
-        // setTimeout( function(){
-        //         $('html').css("overflow-y", "scroll");
-        //     },1000)
-    } else {
-        
-    }
-});
 
 
-// setTimeout( function(){
-//     $('html').css("overflow-y", "scroll");
-// },100)
+setTimeout( function(){$('html').css("overflow-y", "hidden")},0)
+
+setTimeout( function(){$('html').css("overflow-y", "scroll")},3500)
+
+window.onload = function() {
+    setTimeout (function () {
+     scrollTo(0,0);
+    }, 100); //100ms for example
+   }
 
 // Se fetchea la API para ser consumida
 fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
